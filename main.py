0199ff194f781480194a78cc54cb5cfb3e60330c
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-20 00:02:59 +0200

    додала код

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-# Завдання 1
+# Завдання 2
 # Користувач вводить з клавіатури значення у список.
-# Після чого запускаються два потоки. Перший потік знаходить максимум у списку.
-# Другий потік знаходить мінімум
-# у списку. Результати обчислень виведіть на екран.
+# Після чого запускаються два потоки. Перший потік знаходить суму елементів у списку.
+# Другий потік знаходить середнє арифметичне у списку. Результати обчислень
+# виведіть на екран.
 
 import threading
 
 user_input = input("Введіть значення у список (розділіть числа пробілами): ")
-numbers = [int(x) for x in user_input.split()]
+numbers = [float(x) for x in user_input.split()]
 
-# Функція для знаходження максимуму у списку
-def find_max(numbers):
-    global max_result
-    max_result = max(numbers)
+# Функція для знаходження суми елементів у списку
+def find_summ(numbers):
+    global summ_result
+    summ_result = sum(numbers)
 
-# Функція для знаходження мінімуму у списку
-def find_min(numbers):
-    global min_result
-    min_result = min(numbers)
+# Функція для знаходження середнього арифметичного у списку
+def find_aryfmet(numbers):
+    global aryfmet_result
+    aryfmet_result = sum(numbers) / len(numbers)
 
 # Створення потоків
-max_thread = threading.Thread(target=find_max, args=(numbers,))
-min_thread = threading.Thread(target=find_min, args=(numbers,))
+summ_thread = threading.Thread(target=find_summ, args=(numbers,))
+aryfmet_thread = threading.Thread(target=find_aryfmet, args=(numbers,))
 
-max_thread.start()
-min_thread.start()
-max_thread.join()
-min_thread.join()
+summ_thread.start()
+aryfmet_thread.start()
+summ_thread.join()
+aryfmet_thread.join()
 
-print("Максимум у списку:", max_result)
-print("Мінімум у списку:", min_result)
+print("Сума елементів у списку:", summ_result)
+print("Середнє арифметичне у списку:", aryfmet_result)
