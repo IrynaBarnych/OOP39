d4d8ffcabcff1358bf23ca3396ffc57e9f8793f7
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-27 22:37:49 +0200

    додала код

--- main.py
+++ main.py
@@ -7,38 +7,6 @@
 # у списку. Отриманий список, сума та середнє арифметичне
 # виводяться на екран.
 
-import threading
-import random
 
-number_list = []
-lock = threading.Lock()
-list_filled_event = threading.Event()
 
-def fill_list():
-    global number_list
-    with lock:
-        number_list = [random.randint(1, 100) for _ in range(10)]
-    list_filled_event.set()
-
-def calculate_sum():
-    global number_list
-    with lock:
-        return sum(number_list)
-
-def calculate_average():
-    global number_list
-    with lock:
-        return sum(number_list) / len(number_list)
-
-fill_thread = threading.Thread(target=fill_list)
-fill_thread.start()
-
-list_filled_event.wait()
-
-sum_result = calculate_sum()
-average_result = calculate_average()
-
-print("Згенерований список:", number_list)
-print("Сума елементів у списку:", sum_result)
-print("Середнє арифметичне значення у списку:", average_result)
 
