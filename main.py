9ba09c5a115556d96253125a62a166d94f998ffc
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-27 22:28:44 +0200

    додала код

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
-# Завдання 3
+# Завдання 1
+# При старті додатку запускаються три потоки. Перший
+# потік заповнює список випадковими числами. Два інші потоки
+# очікують на заповнення. Коли перелік заповнений, обидва
+# потоки запускаються. Перший потік знаходить суму елементів
+# списку, другий потік знаходить середнє арифметичне значення
+# у списку. Отриманий список, сума та середнє арифметичне
+# виводяться на екран.
 
-"""Користувач вводить з клавіатури шлях до файлу, що
-містить набір чисел. Після чого запускаються два потоки.
-Перший потік створює новий файл, в який запише лише
-парні елементи списку. Другий потік створює новий файл,
-в який запише лише непарні елементи списку. Кількість
-парних і непарних елементів виводиться на екран.
-"""
 import threading
-count_even_numbers = 0
-count_odd_numbers = 0
-#отримання шлях від користувачя
-file_path = input("Введіть шлях до файлу: ")
-#зчитуємо файл
-with open(file_path, "r") as file:
-    numbers = [int(line.strip()) for line in file.readlines()]
-#функція для запису парних чисел
-def write_even_numbers(numbers, file_name):
-    even_numbers = [str(num) + "\n" for num in numbers if not num & 1]
-    global count_even_numbers
-    count_even_numbers = len(even_numbers)
-    with open(file_name, "w") as file:
-        file.writelines(even_numbers)
-    return len(even_numbers)
+import random
 
-#функція для запису непарних чисел
-def write_odd_numbers(numbers, file_name):
-    odd_numbers = [str(num) + "\n" for num in numbers if num & 1]
-    global count_odd_numbers
-    count_odd_numbers = len(odd_numbers)
-    with open(file_name, "w") as file:
-        file.writelines(odd_numbers)
-    return len(odd_numbers)
+number_list = []
+lock = threading.Lock()
+list_filled_event = threading.Event()
 
-#створення потоків
-even_thread = threading.Thread(target=write_even_numbers, args=(numbers, "even_numbers.txt"))
-odd_thread = threading.Thread(target=write_odd_numbers, args=(numbers, "odd_numbers.txt"))
+def fill_list():
+    global number_list
+    with lock:
+        number_list = [random.randint(1, 100) for _ in range(10)]
+    list_filled_event.set()
 
-even_thread.start()
-odd_thread.start()
-even_thread.join()
-odd_thread.join()
+def calculate_sum():
+    global number_list
+    with lock:
+        return sum(number_list)
 
-print("Кількість парних чисел ", count_even_numbers)
-print("Кількість непарних чисел ", count_odd_numbers)
+def calculate_average():
+    global number_list
+    with lock:
+        return sum(number_list) / len(number_list)
+
+fill_thread = threading.Thread(target=fill_list)
+fill_thread.start()
+
+list_filled_event.wait()
+
+sum_result = calculate_sum()
+average_result = calculate_average()
+
+print("Згенерований список:", number_list)
+print("Сума елементів у списку:", sum_result)
+print("Середнє арифметичне значення у списку:", average_result)
+
